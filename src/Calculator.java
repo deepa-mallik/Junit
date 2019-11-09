7ed5ceb03c0c2e7d3b4f38c10bdafa5e6be5c81d
deepa-mallik <57281904+deepa-mallik@users.noreply.github.com>
2019-11-09 14:46:37 +0530

    Add files via upload

---
+++ src/Calculator.java
@@ -0,0 +1,31 @@
+//A unit test is a piece of code written by a developer that executes 
+//a specific functionality in the code to be tested.
+public class Calculator {
+	public int square(int i) 
+	{
+	return i*i;
+	}
+
+	public String user(String string) 
+	{
+		return "sandip";
+	}
+
+	public String concat(String string, String string2) {
+		
+		return string+string2;
+	}
+
+	public int sub(int i, int j) {
+		 		return i-j;
+	}
+
+	public int add(int i, int j) {
+		 		return i+j;
+	}
+
+	public static boolean isSafe() {
+		 		return false;
+	}
+
+}
